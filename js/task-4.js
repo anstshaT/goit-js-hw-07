3bf4974859edea21fcdf946b97b765c8302d2083
anastasiiat <nastiatab11@gmail.com>
2024-11-07 02:26:29 +0100

    Update task four file

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,21 @@
+const formEl = document.querySelector(".login-form");
 
+formEl.addEventListener("submit", handleForm);
+
+function handleForm(event) {
+    event.preventDefault(); 
+
+    const elements = event.target.elements;
+
+    const info = {
+        email: elements.email.value.trim(),
+        password: elements.password.value.trim()
+    };
+
+    if (info.email === "" || info.password === "") {
+        alert(`All form fields must be filled in`);
+    } else {
+        console.log(info);
+        event.target.reset();
+    }
+}
