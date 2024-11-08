8d2325b8e5bce78d7e4640ef75b168cb8cf0bbfc
anastasiiat <nastiatab11@gmail.com>
2024-11-08 16:31:44 +0100

    Update task five task

--- js/task-5.js
+++ js/task-5.js
@@ -1,3 +1,15 @@
+const backgroundBody = document.querySelector("body");
+const changeColBtn = document.querySelector(".change-color");
+const spanEl = document.querySelector(".color");
+
+changeColBtn.addEventListener("click", handelBtn);
+
+function handelBtn() {
+  backgroundBody.style.backgroundColor = getRandomHexColor();
+  spanEl.textContent = getRandomHexColor();
+}
+
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
